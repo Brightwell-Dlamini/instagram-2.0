16e872308e9a1e86695640c2646acd348ac42e65
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-13 00:16:06 +0200

    Header Complete

--- components/Header.js
+++ components/Header.js
@@ -10,11 +10,12 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 function Header() {
   return (
-    <div className="">
-      <div className="flex max-w-6xl justify-between border-red-500">
+    <div className="sticky top-0 z-50 border-b bg-white shadow-sm">
+      <div className="mx-5 flex  max-w-6xl justify-between xl:mx-auto ">
         <div className="relative hidden w-24  cursor-pointer lg:inline-grid">
           <Image src={logo} layout="fill" objectFit="contain" />
         </div>
@@ -36,6 +37,25 @@ function Header() {
         </div>
 
         {/* Right Header */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navButton" />
+          <MenuIcon className="h-6 cursor-pointer md:hidden" />
+          <div className="navButton relative">
+            <PaperAirplaneIcon className="navButton rotate-45" />
+            <div className="absolute -top-1 -right-2 flex h-5 w-5 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              3
+            </div>
+          </div>
+
+          <PlusCircleIcon className="navButton" />
+          <UserGroupIcon className="navButton" />
+          <HeartIcon className="navButton" />
+          <img
+            src="profilePic.jpg"
+            alt="profile pic"
+            className="h-10 cursor-pointer rounded-full"
+          />
+        </div>
       </div>
     </div>
   )
